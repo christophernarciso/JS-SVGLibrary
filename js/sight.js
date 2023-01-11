fe4ab330293683d2ac53e360d886696b5dca6d04
Christopher Narciso <34075885+christophernarciso@users.noreply.github.com>
2023-01-11 17:23:21 -0500

    work

---
+++ js/sight.js
@@ -0,0 +1,31 @@
+class SVGElement {
+    constructor(type) {
+        this.type = type;
+        this.namespace = 'http://www.w3.org/2000/svg';
+        this.node = document.createElementNS(this.namespace, this.type);
+        return this;
+    }
+
+    attr(attrs) {
+        for (const {key, value} of Object.entries(attrs)) {
+            this.node.setAttributeNS(null, key, value);
+        }
+        return this;
+    }
+
+    append(element) {
+        const parent = document.querySelector(typeof element === 'string' ? element : element.node);
+        parent.append(this.node);
+        return this;
+    }
+
+
+}
+
+class Sight {
+    constructor(selector, width, height) {
+        this.selector = selector;
+        this.width = width;
+        this.height = height;
+    }
+}
